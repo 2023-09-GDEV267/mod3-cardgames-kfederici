97a71e46cd2d42754af6b49ad47b560118d3aef7
kfederici <143634514+kfederici@users.noreply.github.com>
2023-12-04 10:31:45 -0500

    update

--- Assets/golf/Scripts/GScoreManager.cs
+++ Assets/golf/Scripts/GScoreManager.cs
@@ -15,13 +15,13 @@ public enum eGScoreEvent
 public class GScoreManager : MonoBehaviour
 {
     static private GScoreManager S;
-    static public int SCORE_FROM_PREV_ROUND = 0;
+    //static public int SCORE_FROM_PREV_ROUND = 0;
     static public int HIGH_SCORE = 0;
 
     [Header("Set Dynamically")]
     //fields to track score info
-    public int chain = 0;
-    public int scoreRun = 0;
+    //public int chain = 0;
+    //public int scoreRun = 0;
     public int score = 0;
 
     void Awake()
@@ -35,14 +35,14 @@ public class GScoreManager : MonoBehaviour
             Debug.LogError("ERROR: GScoreManager.Awake(): S is already set!");
         }
         //check for a high score in PlayerPrefs
-        if (PlayerPrefs.HasKey("ProspectorHighScore"))
+        if (PlayerPrefs.HasKey("GolfSolitaireHighScore"))
         {
-            HIGH_SCORE = PlayerPrefs.GetInt("ProspectorHighScore");
+            HIGH_SCORE = PlayerPrefs.GetInt("GolfSolitaireHighScore");
         }
         //add the score from last round, which will be >0 if it was a win
-        score += SCORE_FROM_PREV_ROUND;
+        //score += SCORE_FROM_PREV_ROUND;
         //and reset the SCORE_FROM_PREV_ROUND
-        SCORE_FROM_PREV_ROUND = 0;
+        //SCORE_FROM_PREV_ROUND = 0;
     }
     static public void EVENT(eGScoreEvent evt)
     {
@@ -61,17 +61,17 @@ public class GScoreManager : MonoBehaviour
         switch (evt)
         {
             //same things need to happen whether it's a draw, a win, or a loss
-            case eGScoreEvent.draw://drwaing a card
+            //case eGScoreEvent.draw://drwaing a card
             case eGScoreEvent.gameWin://won the round
             case eGScoreEvent.gameLoss://lost the round
-                chain = 0;//resets the score chain
-                score += scoreRun;//resets the score chain
-                scoreRun = 0;//reset scoreRun
+                //chain = 0;//resets the score chain
+                //score += scoreRun;//resets the score chain
+                //scoreRun = 0;//reset scoreRun
                 break;
-            case eGScoreEvent.mine://remove a mine card
-                chain++;//increase the score chain
-                scoreRun += chain;//add score for this card to run
-                break;
+            //case eGScoreEvent.mine://remove a mine card
+               //chain++;//increase the score chain
+                //scoreRun += chain;//add score for this card to run
+                //break;
         }
         //this second switch statement handles round wins and losses
         switch (evt)
@@ -79,16 +79,16 @@ public class GScoreManager : MonoBehaviour
             case eGScoreEvent.gameWin:
                 //if it's a win, add the score to the next round
                 //static fields are not reset by SceneManager.LoadScene()
-                SCORE_FROM_PREV_ROUND = score;
+                //SCORE_FROM_PREV_ROUND = score;
                 print("You won this round! Round score: " + score);
                 break;
             case eGScoreEvent.gameLoss:
                 //if it's a loss, check against the high score
-                if (HIGH_SCORE <= score)
+                if (HIGH_SCORE >= score)
                 {
                     print("You got the high score!  High score: " + score);
                     HIGH_SCORE = score;
-                    PlayerPrefs.SetInt("ProspectorHighScore", score);
+                    PlayerPrefs.SetInt("GolfSolitaireHighScore", score);
                 }
                 else
                 {
@@ -96,11 +96,11 @@ public class GScoreManager : MonoBehaviour
                 }
                 break;
             default:
-                print("score: " + score + " scoreRun:" + scoreRun + " chain:" + chain);
+                print("score: " + score);
                 break;
         }
     }
-    static public int CHAIN { get { return S.chain; } }
+    //static public int CHAIN { get { return S.chain; } }
     static public int SCORE { get { return S.score; } }
-    static public int SCORE_RUN { get { return S.scoreRun; } }
+    //static public int SCORE_RUN { get { return S.scoreRun; } }
 }
